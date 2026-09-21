5db67c4f37ab9d585916e64b94050afa51522a9c
Gustavo Assunção <94201266+GustavoAssuncao02@users.noreply.github.com>
2026-09-21 13:32:05 -0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
+  base: '/VM9-Viagens/',
   plugins: [react()],
   server: { host: '127.0.0.1', port: 5175, strictPort: true },
 });
